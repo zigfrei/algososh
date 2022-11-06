032810bf56934f2838612105104ab49237079d3e
zigfrei <zigfrei.zf@gmail.com>
2022-11-07 02:37:20 +0300

    Updates

--- src/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export const getRandomInt = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
